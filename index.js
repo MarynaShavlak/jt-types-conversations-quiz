4ebfe1f4c0638336732512b603481c06ca3f4fef
Maryna Shavlak <shavlakmaryna@gmail.com>
2024-01-12 18:16:26 +0200

    new html

---
+++ index.js
@@ -0,0 +1,32 @@
+$(document).ready(function () {
+
+  $(".tab").css("display", "none");
+  $("#tab-1").css("display", "flex");
+
+  function run(hideTab, showTab){
+    if(hideTab < showTab){ // If not press previous button
+      // Validation if press next button
+      var currentTab = 0;
+      x = $('#tab-'+hideTab);
+      y = $(x).find("input")
+      for (i = 0; i < y.length; i++){
+        if (y[i].value == ""){
+          $(y[i]).css("background", "#ffdddd");
+          return false;
+        }
+      }
+    }
+
+    // Progress bar
+    for (i = 1; i < showTab; i++){
+      $("#step-"+i).css("opacity", "1");
+    }
+
+    // Switch tab
+    $("#tab-"+hideTab).css("display", "none");
+    $("#tab-"+showTab).css("display", "flex");
+    $("input").css("background", "#fff");
+  }
+})
+
+
