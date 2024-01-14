1e730f4d7598f583e7535edca1c8de092aad024f
Maryna Shavlak <shavlakmaryna@gmail.com>
2024-01-14 21:30:14 +0200

    add answers check start

--- index.js
+++ index.js
@@ -2,15 +2,82 @@ $(document).ready(function () {
   const tabClass = '.tab';
   const indexBtnClass = '.index-btn';
   const indexNextBtnClass = 'index-btn--next';
-  const displayFlex = 'flex';
-  const displayNone = 'none';
 
+  const QUIZ__ANSWER = {
+    question1: 'a',
+    question2: 'c',
+    question3: [
+      'a',
+      'b',
+      'd',
+      'e',
+      'f',
+    ],
+    question4: [
+      'a',
+      "c",
+      'e',
+      'f',
+    ],
+    question5: 'c',
+    question6: 12345,
+    question7: 'baNaNa',
+  };
+
+  const userAnswers = {
+    question1: null,
+    question2: null,
+    question3: null,
+    question4: null,
+    question5: null,
+    question6: null,
+    question7: null,
+  };
   updateQuestionToShow($(tabClass), $('#tab-1'));
+  setFocusedState();
 
   const btns = $(indexBtnClass);
   btns.click(function () {
     handleButtonClick($(this));
   });
+
+  $('.submit-btn').click(function (e) {
+    e.preventDefault();
+    handleFormSubmit();
+  });
+
+  function handleFormSubmit() {
+    updateTextAnswer(7);
+    compareAnswers();
+  }
+
+  function compareAnswers() {
+    const quizAnswersList = Object.values(QUIZ__ANSWER);
+    const userAnswersAList = Object.values(userAnswers);
+    quizAnswersList.forEach((quizAnswer, index) => {
+      console.log('quizAnswer: ', quizAnswer);
+      const userAnswer = userAnswersAList[index];
+      console.log('userAnswer: ', userAnswer);
+      const isTheSame  = quizAnswer === userAnswer;
+      console.log('isTheSame: ', isTheSame);
+      const isQuizAnswerArray = Array.isArray(quizAnswer);
+      if (isQuizAnswerArray) {
+        const isFullAnswer = quizAnswer?.length === userAnswer?.length;
+        console.log('isFullAnswer: ', isFullAnswer);
+      }
+      console.log('__________');
+    });
+  }
+
+  function setFocusedState() {
+    const textInputs = $(':text');
+    textInputs.focus(function () {
+      $(this).addClass('focused');
+    });
+    textInputs.blur(function () {
+      $(this).removeClass('focused');
+    });
+  }
 
   function getTabOrder(tabID) {
     const match = tabID.match(/\d+/);
@@ -24,6 +91,7 @@ $(document).ready(function () {
     const showQuestion = isNextBtn ? currentQuestion + 1 : currentQuestion - 1;
 
     updateProgressBar(isNextBtn, currentQuestion, showQuestion);
+    updateUserAnswersObj(currentQuestion);
     updateQuestionToShow(
       $(`#tab-${currentQuestion}`),
       $(`#tab-${showQuestion}`),
@@ -31,8 +99,8 @@ $(document).ready(function () {
   }
 
   function updateQuestionToShow(questionsToHide, questionToShow) {
-    questionsToHide.css('display', displayNone);
-    questionToShow.css('display', displayFlex);
+    questionsToHide.hide();
+    questionToShow.show();
   }
 
   function updateProgressBar(isNextBtn, current, toShow) {
@@ -43,5 +111,49 @@ $(document).ready(function () {
     } else {
       $(`#step-${current}`).removeClass('step--current');
     }
+  }
+
+  function updateUserAnswersObj(currentQuestion) {
+    const element = $("input[name='question" + currentQuestion + "']").get()[0];
+    const typeAttributeValue = $(element).attr('type');
+    if (typeAttributeValue === 'checkbox') {
+      updateCheckBoxAnswers(currentQuestion);
+    } else if (typeAttributeValue === 'radio') {
+      updateRadioAnswers(currentQuestion);
+    } else {
+      updateTextAnswer(currentQuestion);
+    }
+  }
+
+  function updateRadioAnswers(currentQuestion) {
+    const inputSelector =
+      "input[type='radio'][name='question" + currentQuestion + "']:checked";
+    const radioInput = $(inputSelector);
+    const value = radioInput.val();
+    const questionKey = 'question' + currentQuestion;
+    userAnswers[questionKey] = value;
+  }
+
+  function updateCheckBoxAnswers(currentQuestion) {
+    const chosenVariants = $(
+      "input[type='checkbox'][name='question" + currentQuestion + "']:checked",
+    );
+    if (chosenVariants.length !== 0) {
+      const answers = chosenVariants
+        .map(function () {
+          return $(this).val();
+        })
+        .get();
+      userAnswers['question' + currentQuestion] = answers;
+    }
+  }
+
+  function updateTextAnswer(currentQuestion) {
+    const inputSelector =
+      'input[type="text"][name="question' + currentQuestion + '"]';
+    const textInput = $(inputSelector);
+    const value = textInput.val();
+    const questionKey = 'question' + currentQuestion;
+    userAnswers[questionKey] = value;
   }
 });
